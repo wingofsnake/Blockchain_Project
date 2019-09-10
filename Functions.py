40e05cac70f2418ed981b5aa7ebd5d75a6945804
JHPark <wingofsnake@gmail.com>
2019-09-10 18:07:41 +0900

    Add Function file

---
+++ Functions.py
@@ -0,0 +1,6 @@
+import random
+import os
+import time
+import sys
+
+import requests
--- Project_Decent_Simple.py
+++ Project_Decent_Simple.py
@@ -1,38 +1,34 @@
 #import section
 import sys
 
-#define main two variables
+from concurrent import futures
 
-Hash_list = list()
-Crypto_list = list()
+MAX_WORKERS = 20
 
 #define functions
-
-
 def set_parameter(Parameters):
     """Set parameters with system argument values"""
+
     Parameters['Repeat'] = int(sys.argv[1])
-    Parameters['Repeat'] = int(sys.argv[2])
-    Parameters['Repeat'] = int(sys.argv[3])
-    Parameters['Repeat'] = int(sys.argv[4])
-    Parameters['Repeat'] = int(sys.argv[5])
-    Parameters['Repeat'] = int(sys.argv[6])
-    Parameters['Repeat'] = int(sys.argv[7])
+    Parameters['StaticOrNot'] = int(sys.argv[2])
+    Parameters['DistributionFormat'] = int(sys.argv[3])
+    Parameters['InitialParameter'] = int(sys.argv[4])
+    Parameters['NodeSize'] = int(sys.argv[5])
+    Parameters['ProcessingNumber'] = int(sys.argv[6])
+    Parameters['ReinvestmentParameter'] = int(sys.argv[7])
 
 #Main function
-
-if __name__ == '__name__':
+if __name__ == '__main__':
     #define dictionary for parameters
+
     Parameters = {'Repeat': 0, 'StaticOrNot' : 0,
     'DistributionFormat': 0, 'InitialParameter': 0, 'NodeSize': 0,
     'ProcessingNumber': 0, 'ReinvestmentParameter': 0}
 
-    print (1)
     set_parameter(Parameters)
-    print (2)
+
+    #define main two variables
+    Hash_list = list()
+    Crypto_list = list()
 
 
-    for key in Parameters.keys():
-        print(key, ":". Parameters[key])
-
-    print (4)
